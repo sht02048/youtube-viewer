9e9815935e9fa8f996511c2a562243e4e46ebb63
sht02048 <sht02048@gmail.com>
2024-04-23 16:50:21 +0900

    Chore: set tailwindCss and replace files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import "./main.css";
 import React from "react";
+import App from "./App";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,5 +2,11 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}", "./src/**/*.{ts,tsx}"],
+  theme: {
+    backgroundColor: {
+      primary: "#18181b",
+      white: "#FFFFFF",
+    }
+  },
   plugins: [],
 } satisfies Config;
